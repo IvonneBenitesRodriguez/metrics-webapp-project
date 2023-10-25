406a6714eba1adf578b41d9f1de604e4edfdd10c
Ivonne <ivonnebenites2015@gmail.com>
2023-10-25 00:40:44 -0500

    Chores: update file

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCountriesByContinent, updateCountryData } from '../../redux/actions/countriesActions';
-import './Countries.css';
+import s from './style.module.css';
 
 const Countries = () => {
   const { continent } = useParams();
@@ -31,14 +31,16 @@ const Countries = () => {
 
   return (
     <div>
-      <h1>{capitalizeFirstLetter(continent)}</h1>
-      <input
-        type="text"
-        value={filter}
-        onChange={handleFilterChange}
-        placeholder="Filter by country name..."
-      />
-      <div className="countries-grid">
+      <div className={s.search}>
+        <h1>{capitalizeFirstLetter(continent)}</h1>
+        <input
+          type="text"
+          value={filter}
+          onChange={handleFilterChange}
+          placeholder="Filter by country name..."
+        />
+      </div>
+      <div className={s.countries_container}>
         {filteredCountries.map((country) => (
           <Link
             to={{
@@ -48,7 +50,7 @@ const Countries = () => {
             key={country.cca3}
           >
             <div
-              className="country-item"
+              className={s.country_item}
               onClick={() => handleCountryClick(country)}
               onKeyDown={(e) => {
                 if (e.key === 'Enter' || e.key === '') handleCountryClick(country);
@@ -57,7 +59,10 @@ const Countries = () => {
               role="button"
             >
               <img src={country.flags.png} alt={country.name.common} />
-              <p>{country.name.common}</p>
+              <div className={s.description}>
+                <p>{country.name.common}</p>
+                <p>{country.population}</p>
+              </div>
             </div>
           </Link>
         ))}
