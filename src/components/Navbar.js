93b9b0b5b9b0de4709c1a0323dfcf06828a39b3c
Ivonne <ivonnebenites2015@gmail.com>
2023-10-24 21:13:14 -0500

    Chores: update file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <nav>
-    <NavLink to="/" exact activeclassName="active">Home</NavLink>
+    <NavLink to="/" exact="true" activeclassName="active">Home</NavLink>
+    {' '}
     <NavLink to="/details/1" activeclassName="active">Details</NavLink>
   </nav>
 );
