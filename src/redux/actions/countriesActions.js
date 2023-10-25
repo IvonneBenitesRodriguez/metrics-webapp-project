a1cbbfc355108fc7b076fc6ab3a99799b44dc306
Ivonne <ivonnebenites2015@gmail.com>
2023-10-24 23:26:44 -0500

    Chores: update file

--- src/redux/actions/countriesActions.js
+++ src/redux/actions/countriesActions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const updateCountries = (countries) => ({
   type: 'UPDATE_COUNTRIES',
   payload: countries,
@@ -8,4 +10,16 @@ const updateCountryData = (countryData) => ({
   payload: countryData,
 });
 
-export { updateCountries, updateCountryData };
+const fetchCountriesByContinent = (continent) => async (dispatch) => {
+  try {
+    const response = await axios.get(`https://restcountries.com/v3.1/region/${continent}`);
+    if (response.data && response.data.length > 0) {
+      dispatch(updateCountries(response.data));
+    } else {
+      console.log('Valid information about continent were not retrieved by the API');
+    }
+  } catch (error) {
+    console.log('Error to get the continent information: ', error);
+  }
+};
+export { updateCountries, updateCountryData, fetchCountriesByContinent };
