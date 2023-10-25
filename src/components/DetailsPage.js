d46e31f1e0a314a8346ee036e216c4fd000440e7
Ivonne <ivonnebenites2015@gmail.com>
2023-10-24 21:12:26 -0500

    Chores: update file

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import '../styles/DetailsPage.css';
 
-const DetailsPage = () => (
-  <h2>Details Page</h2>
-);
+const DetailsPage = () => {
+  const countryData = useSelector((state) => state.countries.countryData);
+
+  if (!countryData) {
+    return <div>Loading...</div>;
+  }
+
+  const {
+    name, capital, population, region, languages,
+    area, flags, currencies,
+  } = countryData;
+
+  const commonName = name?.common ?? 'N/A';
+  const capitalName = capital?.[0] ?? 'N/A';
+  const populationCount = population ?? 'N/A';
+  const regionName = region ?? 'N/A';
+  const languageNames = languages ? Object.values(languages).join(', ') : 'N/A';
+  const areaSize = area ?? 'N/A';
+  const countryFlag = flags?.[0] ?? '';
+  const countryCurrency = currencies ? Object.keys(currencies).join(', ') : 'N/A';
+
+  return (
+    <div className="details-container">
+      <div className="details-header">
+        <h1>
+          {commonName}
+        </h1>
+        <img src={countryFlag} alt={`${commonName} Flag`} />
+        <p>
+          Capital:
+          {capitalName}
+        </p>
+        <p>
+          Population:
+          {populationCount}
+        </p>
+        <p>
+          Area:
+          {areaSize}
+        </p>
+        <p>
+          Region:
+          {regionName}
+        </p>
+        <p>
+          Languages:
+          {languageNames}
+        </p>
+        <p>
+          Currency:
+          {countryCurrency}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default DetailsPage;
